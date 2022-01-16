8975d9ae2062f2e5ef5cd6077dcaf12dfcaaa67e
YogiPristiawan <yogistanada252@gmail.com>
2022-01-16 23:03:16 +0700

    server running

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
-// const createServer = require('./api/CreateServer')
-// const FigureCalculator = require('./geometry/FigureCalculator')
-// const MathBasic = require('./math/MathBasic')
+const createServer = require('./api/CreateServer')
+const FigureCalculator = require('./geometry/FigureCalculator')
+const MathBasic = require('./math/MathBasic')
 
-// const init = async () => {
-//   const figureCalculator = new FigureCalculator(MathBasic)
-//   const server = createServer({ mathBasic: MathBasic, figureCalculator })
+const init = async () => {
+  const figureCalculator = new FigureCalculator(MathBasic)
+  const server = createServer({ mathBasic: MathBasic, figureCalculator })
 
-//   await server.start()
-//   console.log(`Serve running on at ${server.info.url}`)
-// }
+  await server.start()
+  console.log(`Serve running on at ${server.info.uri}`)
+}
 
-// init()
+init()
